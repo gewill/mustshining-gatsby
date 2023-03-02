30ea4efbe3df559eea92dc3653a875c5db995fe5
Ge Will <531sunlight@gmail.com>
2023-03-02 12:57:52 +0800

    新增 HeroSection

---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function HeroSection() {
+  return (
+    <>
+      <h1>
+        Must shinning{" "}
+        <span role="img" aria-label="shinning">
+          🎈
+        </span>
+      </h1>
+    </>
+  )
+}
+
+export default HeroSection
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,14 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import HeroSection from "../components/HeroSection"
 
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <HeroSection />
   </Layout>
 )
 
